1a9404ca27da47ac5edd72c697ff7e4221fdcf07
nightblade9 <nightbladecodes@gmail.com>
2019-02-27 17:16:40 -0500

    Reorg: Godot code in subproject Create API C# project with basic class/interface

---
+++ C#/GameApi/AbstractMonster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameApi
+{
+    public abstract class AbstractMonster
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+    }
+}
